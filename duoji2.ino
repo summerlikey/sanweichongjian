31994277abc9dea7966ba90a30f83cf56a6dc143
summerlikey <47977965+summerlikey@users.noreply.github.com>
2019-02-26 10:59:49 +0800

    第一次提交程序
    包含舵机、云台、数据读取程序

---
+++ duoji2.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>    // 声明调用Servo.h库
+Servo myservo1;
+Servo myservo2;
+int pos = 0;
+void setup() {
+myservo1.attach(9);
+myservo2.attach(10);
+Serial.begin(9600);
+}
+void loop() {   
+for(pos = 0; pos < 180; pos += 1){
+myservo1.write(pos);
+myservo2.write(pos);
+delay(45);
+}
+for(pos = 180; pos>=1; pos-=1) {
+myservo1.write(pos);
+myservo2.write(pos);
+delay(45);
+}
+}
+
